73044dddda3deb1529002332a27981d8e7bd54ac
pinnsvin <pinnsvin@163.com>
2020-08-07 17:19:26 +0800

    init

---
+++ tools/src/test/java/MyTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author: dujuntao
+ * @description:
+ * @Date: 2020/8/7
+ */
+
+public class MyTest {
+
+    @Test
+    public void test() {
+
+    }
+}
